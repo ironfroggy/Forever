29aec204259b92f3f3d974b6ad560fd8b0bddc0b
Calvin Spealman <calvin@willow-alpha.(none)>
2009-08-01 16:41:45 -0400

    Add walker stuff to tilearea demo, so I can see it moving on a background.

--- foreverdrive/demos/test_tilearea.py
+++ foreverdrive/demos/test_tilearea.py
@@ -12,6 +12,10 @@ screen.blit(background, (0, 0))
 
 area = TileArea("default_tile.png", (10, 10))
 
+boxes = pygame.sprite.RenderUpdates()
+box = MovingBox((128, 128, 128), (200, 200))
+boxes.add(box)
+
 quit = False
 while not quit:
     #Handle Input Events
@@ -25,9 +29,24 @@ while not quit:
         elif event.type == KEYUP and event.key == K_F10:
             import pdb
             pdb.set_trace()
+        elif event.type == KEYDOWN:
+            if event.key == K_UP:
+                box.move_up()
+            elif event.key == K_DOWN:
+                box.move_down()
+            elif event.key == K_LEFT:
+                box.move_left()
+            elif event.key == K_RIGHT:
+                box.move_right()
+
+    ticks = pygame.time.get_ticks()
 
     tg = area.tile_group
-    tg.update(pygame.time.get_ticks())
+    tg.update(ticks)
+    boxes.update(ticks)
+
     rectlist = tg.draw(screen)
-    pygame.display.update(rectlist)
-    tg.clear(screen, background)
+    pygame.display.update(rectlist + boxes.draw(screen))
+
+#   tg.clear(screen, background)
+#    boxes.clear(screen, background)
