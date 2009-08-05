a085679ef6ba87164e37b7b83a2f6fb07e392bc9
Calvin Spealman <ironfroggy@gmail.com>
2009-08-05 15:57:14 -0400

    dont change water bounds in demo, so we can see the edges properly.

--- demos/test_water.py
+++ demos/test_water.py
@@ -44,10 +44,10 @@ class AreaManagingMode(ScrollingMode):
                     name="water %d" % (x,))
                 #obstruction.image.fill((x*(20+(y%5)), x*(20+(y%3)), x*(20+(y%4)), 100))
                 objects.append(obstruction)
-                obstruction.boundrect.top += 10
-                obstruction.boundrect.left += 10
-                obstruction.boundrect.width -= 10
-                obstruction.boundrect.height -= 10
+#                obstruction.rect.top -= 10
+#                obstruction.rect.left -= 10
+#                obstruction.boundrect.width -= 20
+#                obstruction.boundrect.height -= 20
                 obstruction.pressure = 0.0005
 
         self.make_cylinder(100, 50)
