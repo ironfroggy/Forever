cd2722af16f33b4f9ea91c18de562e8c96c391c3
Calvin Spealman <ironfroggy@gmail.com>
2009-08-11 21:45:53 -0400

    Add room, test_room, and room graphics.

---
+++ demos/test_room.py
@@ -0,0 +1,54 @@
+from foreverdrive.base import ForeverMain
+from foreverdrive.modes.scrolling import ScrollingMode
+from foreverdrive.area import AreaManager
+from foreverdrive.area.room import Room
+from foreverdrive.sprite import Sprite, SolidSprite, FacingSprite, CloudSprite, ImmovableSprite, Player
+from foreverdrive.events import Entering, CancelEvent
+from foreverdrive.media import SpriteSet
+
+cottage_spriteset = SpriteSet("cottage")
+
+class RoomTestMode(ScrollingMode):
+
+    def __init__(self, *args, **kwargs):
+        super(RoomTestMode, self).__init__(*args, **kwargs)
+        self.areas = AreaManager(self, cottage_spriteset, Room)
+
+    def first_entering(self):
+        area = self.areas.new_areas([
+            ((100, 100), (6, 6)),
+            ])[0]
+        self.areas.connect_all()
+        self.areas.decorate_all()
+        self.area = area
+
+        player_sprite_set = SpriteSet("defaults")
+
+        sprite = area.create_sprite(
+            Player,
+            topleft=(200, 100),
+            height=25,
+            spriteset=player_sprite_set,
+            name="player")
+        sprite.register_listeners(self)
+        self.player = sprite
+
+        sprite.grow_part((-25, 0), 'head')
+
+        super(RoomTestMode, self).first_entering()
+
+    def make_cylinder(self, x, y):
+        obj = self.area.create_sprite(
+            SolidSprite,
+            topleft=(x, y),
+            height=30,
+            slotgroup="default", slotname="oildrum",
+            name="block")
+
+
+def main():
+    game = ForeverMain(initmode=RoomTestMode)
+    game.run()
+
+if __name__ == '__main__':
+    main()
---
+++ foreverdrive/area/room.py
@@ -0,0 +1,61 @@
+from foreverdrive.sprite import ImmovableSprite
+from foreverdrive.area import BoundArea
+
+
+class Room(BoundArea):
+    """Defines an area with walls, inside.
+
+    Slotgroup: roomwall
+    Slots: (The directions are named for the direction they *face*
+    - flat_down
+    - flat_up
+    - flat_left
+    - flat_right
+    - concave_upright
+    - concave_upleft
+    - concave_downright
+    - concave_downleft
+    - convex_upright
+    - convex_upleft
+    - convex_downright
+    - convex_downleft
+    """
+
+    def decorate(self):
+        """Adds SolidSprite instances for walls, handling
+        portals properly.
+
+        There is a convex tile on each side of each portal.
+        """
+
+        self.create_sprite(ImmovableSprite, (0, 0), slotgroup="roomwall", slotname="concave_downright")
+
+        left = self.width - self.spriteset.load("roomwall", "concave_downleft").get_width()
+        self.create_sprite(ImmovableSprite, (0, left), slotgroup="roomwall", slotname="concave_downleft")
+
+        upright = self.spriteset.load("roomwall", "concave_upright")
+        top = self.height - upright.get_height()
+        self.create_sprite(ImmovableSprite, (top, 0), slotgroup="roomwall", slotname="concave_upright")
+
+        upleft_width = self.spriteset.load("roomwall", "concave_upleft").get_width()
+        left = self.width - upleft_width
+        self.create_sprite(ImmovableSprite, (top, left), slotgroup="roomwall", slotname="concave_upleft")
+
+        self._decorate_bottom_wall(upright.get_width(), left)
+
+    def _decorate_bottom_wall(self, lower, upper):
+        flat_up = self.spriteset.load("roomwall", "flat_up")
+        flat_up_width = flat_up.get_width()
+        top = self.height - flat_up.get_height()
+        distance = upper - lower
+        left = lower
+        while left + flat_up_width <= upper:
+            self.create_sprite(ImmovableSprite, (top, left), slotgroup="roomwall", slotname="flat_up")
+            left += flat_up_width
+        remaining = upper - left
+        if remaining:
+            last = self.create_sprite(ImmovableSprite, (top, left), slotgroup="roomwall", slotname="flat_up")
+            clip = last.image.get_clip()
+            clip.width = remaining
+            last.image.set_clip(clip)
+        
